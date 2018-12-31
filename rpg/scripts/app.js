dceb2200d5c8dec687cd20ec4dfe5df6718caa04
Ken Snow <k.snowjr@gmail.com>
2018-12-31 14:52:28 -0700

    canvas practice rpg

--- rpg/scripts/app.js
+++ rpg/scripts/app.js
@@ -34,31 +34,32 @@ function Hero(name){
     this.attack = 1
     this.armor = 0
     this.attackSpeed = 3000
-    this.moveHero = function(x, y) {
-        x = this.position[0]
-        y = this.position[1]
-        addEventListener("keydown",(e) => {
-            if (e.keyCode === 87 || e.keyCode === 119){
-                this.position[1]--
-            } else if (e.keyCode === 65 || e.keyCode === 97){
-                this.position[0]--
-            } else if (e.keyCode === 68 || e.keyCode === 100){
-                this.position[0]++
-            } else if (e.keyCode === 83 || e.keyCode === 115){
-                this.position[1]++
-            }
-            console.log (this.position)
-        })
-    }
 }
+
+Hero.prototype.moveHero = function(){
+    addEventListener("keydown",(e) => {
+        if (e.keyCode === 87 || e.keyCode === 119){
+            this.position[1]--
+        } else if (e.keyCode === 65 || e.keyCode === 97){
+            this.position[0]--
+        } else if (e.keyCode === 68 || e.keyCode === 100){
+            this.position[0]++
+        } else if (e.keyCode === 83 || e.keyCode === 115){
+            this.position[1]++
+        }
+        console.log (this.position)
+        
+    })
+}
+
+
+
 
 // heroName = prompt("Hero name?", "Frodo")
 heroName = "Bob"
 const hero = new Hero(heroName)
-hero.moveHero()
-    // heroBody.onload = () => {
-    //     ctx.drawImage(heroBody, 0, 119, 16, 16, xLoc, yLoc, 32, 32)
-    // }
+// hero.moveHero()
+
 
 /*
 TODO:
@@ -81,7 +82,8 @@ TODO:
 
 
 function gameStateHandler(){
-    drawMap()
+    setInterval(drawMap, 30)
+    setInterval(clearMap, 30)
     //if there is no char name, it must be a new game.  Prompt user for name
     // if (!charachterName){
     //     charachterName = prompt("Please enter charachter name", "Frodo");
@@ -93,12 +95,21 @@ function gameStateHandler(){
 
 }
 
-function drawMap(){
+clearMap = function(){
+    const canvas = document.querySelector("#hud")
+    var ctx = canvas.getContext("2d")
+
+    ctx.clearRect(0,0, canvas.height, canvas.width)
+}
+
+drawMap = function(){
 
     const canvas = document.querySelector("#hud")
     var ctx = canvas.getContext("2d")
     
-    // grass.onload = () => {
+    drawGrass(ctx);
+    drawHero(ctx);
+    // drawGrass = () => {
     //     for(i = 0; i <= canvasSize; i += 64){
     //         for (n = 0; n <= canvasSize; n += 64){
     //             // console.log ("i: "+i + " n: "+n)
@@ -107,18 +118,32 @@ function drawMap(){
     //     }
         
     // }
+  
     // ctx.fillStyle = 'rgb(120, 50, 10)';
     // ctx.fillRect(0, 0, 600, 600);
 
     // ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
     // ctx.fillRect(30, 30, 50, 50);
+    // ctx.clearRect(0,0,600,600);
 }
 
+function drawGrass (ctx){
+    for(i = 0; i <= canvasSize; i += 64){
+        for (n = 0; n <= canvasSize; n += 64){
+            // console.log ("i: "+i + " n: "+n)
+            ctx.drawImage(grass, i, n, 64, 64)
+        }
+    }
+}
+function drawHero (ctx){
+    console.log("Haroo!")
+    ctx.drawImage(heroBody, 0, 119, 16, 16, hero.position[0], hero.position[1], 32, 32)
+
+}
 // function movePlayer(){
 
 //     })
 // }
 
-function buttonHandler (){}
     
 window.onload = gameStateHandler();
